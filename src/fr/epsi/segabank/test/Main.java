9da890f827fe6e8a308929213de233b802ddd339
LeNico44 <nicolas.houisse@epsi.fr>
2018-07-16 14:15:02 +0200

    création des packages !!

---
+++ src/fr/epsi/segabank/test/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package fr.epsi.segabank.test;
+
+/**
+ * @author nicolas
+ *
+ */
+public class Main {
+
+	/**
+	 * 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("La Banque de monsieur Sega !");
+	}
+
+}
