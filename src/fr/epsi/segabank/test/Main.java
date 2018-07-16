71e97d999684e4d38ae9320201022601f4f7d60e
LeNico44 <nicolas.houisse@epsi.fr>
2018-07-16 18:03:18 +0200

    Et voilà ça marche ! la base de données est créées !!!!!

--- src/fr/epsi/segabank/test/Main.java
+++ src/fr/epsi/segabank/test/Main.java
@@ -3,6 +3,10 @@
  */
 package fr.epsi.segabank.test;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 /**
  * @author nicolas
  *
@@ -14,6 +18,10 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("SegaBank");
+		EntityManager em = emf.createEntityManager();
+		
 		System.out.println("La Banque de monsieur Sega !");
 	}
 
